c54b5effb70611e9864fd03c7a611d14138a906f
hgoel3105 <hgoel3105@gmail.com>
2025-02-04 23:22:28 +0530

    assignment3

---
+++ CP/Harshil/assignment3/ATCODER-Money_sums.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n, w;
+    cin >> n >> w;
+    vector<int> wt(n), val(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> wt[i] >> val[i];
+    }
+    vector<vector<long long>> dp(n+1, vector<long long>(w+1, 0));
+ 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= w; j++)
+        {
+            if(j >= wt[i-1])
+            {
+                dp[i][j] = max(val[i-1] + dp[i-1][j - wt[i-1]], dp[i-1][j]);
+            } else 
+            {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    cout << dp[n][w] << endl;
+    return 0;
+}
---
+++ CP/Harshil/assignment3/ATCODER-Vacation.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n][3];
+    for(int i=0;i<n;i++)
+    {
+        
+    }
+}
